20efb23e6572562e6e80e11259828fff5a020df7
@CodebyOmar <omarmohammed580@gmail.com>
2017-11-12 11:29:28 -0800

    final submission

---
+++ src/components/studentProfile/profile.vue
@@ -0,0 +1,23 @@
+<template src="./profile.html">
+</template>
+
+<script>
+import mainTab from "@/components/tab/mainTab"
+
+  export default {
+    name:"studentProfile",
+    props:["student"],
+    computed: {
+      student_ () {
+        let res = JSON.parse(this.student);
+        return res
+      }
+    },
+    methods:{
+      goBack(){
+        return window.history.back()
+      }
+    },
+    components:{mainTab}
+  }
+</script>
