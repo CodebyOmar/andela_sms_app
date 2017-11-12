5ca131bb085f2414536765cd9d08c2ee84c4ecde
@CodebyOmar <omarmohammed580@gmail.com>
2017-11-12 11:20:34 -0800

    admin page added

---
+++ src/components/admin/students/students.vue
@@ -0,0 +1,27 @@
+<template src="./students.html">
+</template>
+
+<script>
+import {mapGetters, mapActions} from "vuex"
+import FadeLoader from "../../../../node_modules/vue-spinner/src/FadeLoader"
+  export default {
+    name:"students",
+    created: function () { return this.getStudents() },
+    data () {
+      return {
+        loading:true
+      }
+    },
+    computed:{
+      ...mapGetters(['allStudents'])
+    },
+    methods: {
+      ...mapActions({getStudents:"getStudents",deleteStudent:"deleteStudent"}),
+      async delStudent(regnum){
+        this.deleteStudent(regnum)
+        return this.getStudents()
+      }
+    },
+    components:{FadeLoader}
+  }
+</script>
